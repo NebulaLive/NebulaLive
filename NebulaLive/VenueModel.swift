c2e6649dcdd758835b158d2988c217de9c5b54aa
Kyle Thomas <eeu479@gmail.com>
2018-08-02 21:30:38 +0100

    ADDED realm migration

--- NebulaLive/VenueModel.swift
+++ NebulaLive/VenueModel.swift
@@ -7,11 +7,35 @@
 //
 
 import Foundation
+import Realm
+import RealmSwift
 
-struct VenueModel {
-    var name: String
-    var desc: String
-    var image: String
-    var rating: Int
-    var distance: Int
+class VenueModel: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var desc: String = ""
+    @objc dynamic var image: String = ""
+    @objc dynamic var rating: Int = 0
+    @objc dynamic var distance: Int = 0
+    
+    init(name: String, desc: String, image: String, rating: Int, distance: Int) {
+        self.name = name
+        self.desc = desc
+        self.image = image
+        self.rating = rating
+        self.distance = distance
+        super.init()
+    }
+    
+    
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+    
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        fatalError("init(realm:schema:) has not been implemented")
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        fatalError("init(value:schema:) has not been implemented")
+    }
 }
